8c0ac2a8c39f1cc0b3e1253f1ac86ea1fac6d2ed
ataerg <ataerg@github.com>
2022-06-01 00:23:37 +0200

    Corrected error on apply chat

--- src/app/chatting/apply-chat/apply-chat.component.ts
+++ src/app/chatting/apply-chat/apply-chat.component.ts
@@ -36,24 +36,20 @@ export class ApplyChatComponent implements OnInit {
       console.log(form_values['date']);
       console.log(this.applyMsg.valid);
       if (this.applyMsg.valid) {
-        let year = form_values['date'].slice(0, 4);
-        let mes =  form_values['date'].slice(5, 7);
-        let dia =  form_values['date'].slice(8, 10);
-        let converted_date = dia + "/" + mes + "/" + year;
-        let date = new Date(year + '-' + mes + '-' + dia);
+        let dia = form_values['date'].slice(0, 2);
+        let mes =  form_values['date'].slice(3, 5);
+        let year =  form_values['date'].slice(6, 10);
+        let date = new Date(year,mes-1,dia);
         let today = new Date();
         if (date < today) {
           alert("Error! La fecha debe ser posterior a la fecha actual");
           return;
         }
-        console.log(form_values['admin_id']);
-        console.log(converted_date);
-        console.log(form_values['time']);
-        this.appointmentService.getAdminsAppointmentsWithDateTime(form_values['admin_id'], converted_date, form_values['time']).subscribe({
+        this.appointmentService.getAdminsAppointmentsWithDateTime(form_values['admin_id'], form_values['date'], form_values['time']).subscribe({
           next: (res) => {
             if (res.length == 0) {
               console.log(res);
-              this.appointmentService.createAppointment(this.user_id, form_values['admin_id'], converted_date, form_values['time'])
+              this.appointmentService.createAppointment(this.user_id, form_values['admin_id'], form_values['date'], form_values['time'])
                 .subscribe({
                   next: () => {
                     alert("Cita creada con éxito");
@@ -71,22 +67,6 @@ export class ApplyChatComponent implements OnInit {
           error: (er: any)=> console.log(er)
         })
       }
-      /*
-      this.appointmentService.createAppointment(this.user_id, this.admins[random_admin], converted_date, this.applyMsg.value.time)
-      .subscribe({
-        next: () => {
-          /*
-          this.router.navigate(['/chat']).then(() => {
-            window.location.reload();
-          });
-
-        },
-        error: (er: any)=> console.log(er)
-      });
-    } else {
-      alert("Error! No se puede asignar una cita");
-    }
-    */
     }
   }
 }
